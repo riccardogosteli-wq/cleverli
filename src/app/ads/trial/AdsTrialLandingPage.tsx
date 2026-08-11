1f268acfdffa4a20d48d3f8e8f52031614165f2a
Ricci <riccardogosteli@riccis-macbook-pro.home>
2026-08-11 16:27:28 +0200

    Revert "Add ad landing page exercise image assets"
    This reverts commit 8391dd41cb52d786bd074c846745178b707e787f.

--- src/app/ads/trial/AdsTrialLandingPage.tsx
+++ src/app/ads/trial/AdsTrialLandingPage.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { trackAdsLpCtaClick } from "@/lib/analytics";
 import { startCheckout } from "@/lib/checkoutClient";
 import { useSession } from "@/hooks/useSession";
-import AdsExerciseImages from "@/app/ads/components/AdsExerciseImages";
 import GameExercisePreview from "@/app/ads/components/GameExercisePreview";
 
 type Plan = "monthly" | "yearly";
@@ -131,8 +130,6 @@ export default function AdsTrialLandingPage({
       </section>
 
       <GameExercisePreview />
-
-      <AdsExerciseImages />
 
       <section className="bg-white px-4 py-12 sm:px-6 sm:py-16">
         <div className="mx-auto max-w-5xl">
