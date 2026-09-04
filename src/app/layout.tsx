fc019a16f67e7aa503920829daec64feb2fb7fac
Ricci <riccardogosteli@gmail.com>
2026-09-04 17:55:18 +0200

    Improve PWA app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,8 +79,8 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon.ico" },
-      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/app-icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/app-icon-512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
